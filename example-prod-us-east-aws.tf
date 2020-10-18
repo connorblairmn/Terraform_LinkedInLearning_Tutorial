67866dc79492591659fd17db9a6a3c349a5decd1
connorblairmn <connorblairmn@gmail.com>
2020-10-18 08:29:57 -0500

    Added security group

--- example-prod-us-east-aws.tf
+++ example-prod-us-east-aws.tf
@@ -10,3 +10,34 @@ resource "aws_s3_bucket" "prod_tf_course" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_security_group" "prod_web" {
+  name       = "prod_web"
+  description= "Allow standard http and https ports inbound and everything outbound."
+  //http input from any IP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  //https input from any IP 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  //output to anywhere with any protocol
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+  
+}
+
